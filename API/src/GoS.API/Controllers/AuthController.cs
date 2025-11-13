77d7dab1728b7751e5c353475b01e6fa53f7cac0
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-11-14 00:35:22 +0300

    feat: optimized the way by getting claims

--- API/src/GoS.API/Controllers/AuthController.cs
+++ API/src/GoS.API/Controllers/AuthController.cs
@@ -27,51 +27,9 @@ public class AuthController : ControllerBase
     [HttpGet("profile")]
     public IActionResult GetProfile()
     {
-        var user = User;
-        
-        var steamId64 = user.FindFirst(SteamClaimTypes.SteamId64)?.Value;
-        var steamId32 = user.FindFirst(SteamClaimTypes.SteamId32)?.Value;
-        var profileUrl = user.FindFirst(SteamClaimTypes.ProfileUrl)?.Value;
-        var steamName = user.FindFirst(SteamClaimTypes.SteamName)?.Value;
-        var realName = user.FindFirst(SteamClaimTypes.RealName)?.Value;
-        
-        var avatarSmall = user.FindFirst(SteamClaimTypes.Avatar)?.Value;
-        var avatarMedium = user.FindFirst(SteamClaimTypes.AvatarMedium)?.Value;
-        var avatarFull = user.FindFirst(SteamClaimTypes.AvatarFull)?.Value;
-        
-        var timeCreatedClaim = user.FindFirst(SteamClaimTypes.TimeCreated)?.Value;
-        var lastLogoffClaim = user.FindFirst(SteamClaimTypes.LastLogoff)?.Value;
-        
-        DateTime? profileCreated = null;
-        DateTime? lastLogoff = null;
-        
-        if (long.TryParse(timeCreatedClaim, out var timeCreatedLong))
-        {
-            profileCreated = DateTimeOffset.FromUnixTimeSeconds(timeCreatedLong).DateTime;
-        }
-        
-        if (long.TryParse(lastLogoffClaim, out var lastLogoffLong))
-        {
-            lastLogoff = DateTimeOffset.FromUnixTimeSeconds(lastLogoffLong).DateTime;
-        }
-
-        return Ok(new
-        {
-            IsAuthenticated = user.Identity?.IsAuthenticated ?? false,
-            SteamId64 = steamId64,
-            SteamId32 = steamId32,
-            SteamName = steamName,
-            ProfileUrl = profileUrl,
-            RealName = realName,
-            Avatar = new 
-            {
-                Small = avatarSmall,
-                Medium = avatarMedium,
-                Full = avatarFull
-            },
-            ProfileCreated = profileCreated,
-            LastLogoff = lastLogoff,
-        });
+        return Ok(User.Claims
+            .Where(x => x.Type.StartsWith(SteamAuthenticationDefaults.AuthenticationScheme))
+            .ToDictionary(x => x.Type.ToLowerInvariant(), x => x.Value));
     }
 
     [HttpGet("status")]
--- API/src/Infrastructure/GoS.Infrastructure.Steam/SteamClaimTypes.cs
+++ API/src/Infrastructure/GoS.Infrastructure.Steam/SteamClaimTypes.cs
@@ -2,14 +2,14 @@ namespace GoS.Infrastructure.Steam;
 
 public static class SteamClaimTypes
 {
-    public const string SteamId64 = "steam_id64";
-    public const string SteamId32 = "steam_id32";
-    public const string ProfileUrl = "steam_profileurl";
-    public const string Avatar = "steam_avatar";
-    public const string AvatarMedium = "steam_avatarmedium";
-    public const string AvatarFull = "steam_avatarfull";
-    public const string SteamName = "steam_steamname";
-    public const string RealName = "steam_realname";
-    public const string TimeCreated = "steam_timecreated";
-    public const string LastLogoff = "steam_lastlogoff";
+    public const string SteamId64 = $"{SteamAuthenticationDefaults.AuthenticationScheme}_id64";
+    public const string SteamId32 = $"{SteamAuthenticationDefaults.AuthenticationScheme}_id32";
+    public const string ProfileUrl = $"{SteamAuthenticationDefaults.AuthenticationScheme}_profileurl";
+    public const string Avatar = $"{SteamAuthenticationDefaults.AuthenticationScheme}_avatar";
+    public const string AvatarMedium = $"{SteamAuthenticationDefaults.AuthenticationScheme}_avatarmedium";
+    public const string AvatarFull = $"{SteamAuthenticationDefaults.AuthenticationScheme}_avatarfull";
+    public const string SteamName = $"{SteamAuthenticationDefaults.AuthenticationScheme}_steamname";
+    public const string RealName = $"{SteamAuthenticationDefaults.AuthenticationScheme}_realname";
+    public const string TimeCreated = $"{SteamAuthenticationDefaults.AuthenticationScheme}_timecreated";
+    public const string LastLogoff = $"{SteamAuthenticationDefaults.AuthenticationScheme}_lastlogoff";
 }
