e2f5ed7a5b0bc8716e20d87a415150e5df5a7309
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-11-22 12:14:00 +0300

    fix: removed unused fields in the LeagueDto.cs

--- API/src/GoS.Application/Features/Common/Queries/GetLeagues/LeagueDto.cs
+++ API/src/GoS.Application/Features/Common/Queries/GetLeagues/LeagueDto.cs
@@ -6,8 +6,6 @@ namespace GoS.Application.Features.Common.Queries.GetLeagues;
 public class LeagueDto
 {
     public long LeagueId { get; init; }
-    public string Ticket { get; init; } = string.Empty;
-    public string Banner { get; init; } = string.Empty;
     public BaseEnumDto<Tier>? Tier { get; init; }
     public string Name { get; init; } = string.Empty;
 }
