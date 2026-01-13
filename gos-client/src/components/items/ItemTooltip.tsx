7f6cc0b972e8fdf6e92286d1c40feac94dda124f
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2026-01-13 13:34:04 +0300

    fix: icon fixes & backend items.json fix

--- gos-client/src/components/items/ItemTooltip.tsx
+++ gos-client/src/components/items/ItemTooltip.tsx
@@ -228,12 +228,11 @@ export const ItemTooltip: React.FC<Props> = ({ itemName, children }) => {
                                                     {item.mc && (
                                                         <div className="flex items-center gap-0.5 text-[#0099ff] font-bold text-xs">
                                                             <Icon src="/assets/images/ability_manacost.png" size={4} />
-                                                            <div className="w-2 h-2 bg-[#0099ff] rounded-sm"></div>
                                                             {item.mc}
                                                         </div>
                                                     )}
                                                     {item.hc && (
-                                                        <div className="flex items-center gap-0.5 text-[#286323] font-bold text-xs">
+                                                        <div className="flex items-center gap-0.5 text-[#e03e2e] font-bold text-xs">
                                                             <Icon src="/assets/images/ability_healthcost.png" size={4} />
                                                             {item.hc}
                                                         </div>
